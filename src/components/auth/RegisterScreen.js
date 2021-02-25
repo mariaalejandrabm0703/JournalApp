a726678a7d1d537f5d492431b4fd9522f974c0e6
Maria <mariaalejandrabm0703@gmail.com>
2021-02-25 16:15:42 -0500

    Se configura useForm en el registro del usuario.

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,61 +1,76 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
 
 export const RegisterScreen = () => {
-    return (
-        <>
-            <h3 className="auth__title">Register</h3>
-
-            <form>
-
-                <input 
-                    type="text"
-                    placeholder="Name"
-                    name="name"
-                    className="auth__input"
-                    autoComplete="off"
-                />
-
-                <input 
-                    type="text"
-                    placeholder="Email"
-                    name="email"
-                    className="auth__input"
-                    autoComplete="off"
-                />
-
-                <input 
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    className="auth__input"
-                />
-
-                <input 
-                    type="password"
-                    placeholder="Confirm password"
-                    name="password2"
-                    className="auth__input"
-                />
 
 
-                <button
-                    type="submit"
-                    className="btn btn-primary btn-block mb-5"
-                >
-                    Register
-                </button>
+  const [formValues, handleInputChange, reset] = useForm({
+    name: "",
+    email: "",
+    password:"",
+    password2:"",
+  });
 
-               
+  const {name, email, password, password2} = formValues
 
-                <Link 
-                    to="/auth/login"
-                    className="link"
-                >
-                    Already registered?
-                </Link>
+  const handleSumit = (e) =>{
+    e.preventDefault();
+    console.log(name, email, password, password2)
+    //reset();
+  }
 
-            </form>
-        </>
-    )
-}
+  return (
+    <>
+      <h3 className="auth__title">Register</h3>
+
+      <form onSubmit={handleSumit}>
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          value={name}
+          className="auth__input"
+          autoComplete="off"
+          onChange={handleInputChange}
+        />
+
+        <input
+          type="text"
+          placeholder="Email"
+          name="email"
+          value={email}
+          className="auth__input"
+          autoComplete="off"
+          onChange={handleInputChange}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={password}
+          className="auth__input"
+          onChange={handleInputChange}
+        />
+
+        <input
+          type="password"
+          placeholder="Confirm password"
+          name="password2"
+          value={password2}
+          className="auth__input"
+          onChange={handleInputChange}
+        />
+
+        <button type="submit" className="btn btn-primary btn-block mb-5">
+          Register
+        </button>
+
+        <Link to="/auth/login" className="link">
+          Already registered?
+        </Link>
+      </form>
+    </>
+  );
+};
