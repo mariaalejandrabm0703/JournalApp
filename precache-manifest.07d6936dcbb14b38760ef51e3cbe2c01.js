0b88d84a8e81febad947807cc4acb06a0bbc26fc
Maria Bohorquez <mariaalejandrabm0703@gmail.com>
2021-03-17 17:26:55 -0500

    Deploy

---
+++ precache-manifest.07d6936dcbb14b38760ef51e3cbe2c01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6546d7ebc42c96b4421b1b015134575b",
+    "url": "/JournalApp/index.html"
+  },
+  {
+    "revision": "7154efec79c5eb12fb7d",
+    "url": "/JournalApp/static/css/main.efee262e.chunk.css"
+  },
+  {
+    "revision": "88ffb1151e225d85c491",
+    "url": "/JournalApp/static/js/2.05f02a6a.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/JournalApp/static/js/2.05f02a6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7154efec79c5eb12fb7d",
+    "url": "/JournalApp/static/js/main.d8630187.chunk.js"
+  },
+  {
+    "revision": "5a67d94bb35268e32caf",
+    "url": "/JournalApp/static/js/runtime-main.57020a02.js"
+  }
+]);
